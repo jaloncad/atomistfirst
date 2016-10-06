bc0992c4221aee94cb2d97ab9d9d30fe613f2b5a
Juan Alonso Cadierno <yoel.abril@gmail.com>
2016-10-06 23:17:12 +0200

    Created by Atomist project editor atomist-project-templates.spring-boot-common-editors.AddHystrix

--- src/main/java/es/jaloncad/demo/HelloServiceApplication.java
+++ src/main/java/es/jaloncad/demo/HelloServiceApplication.java
@@ -2,8 +2,10 @@ package es.jaloncad.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class HelloServiceApplication {
 
     public static void main(String[] args) {
